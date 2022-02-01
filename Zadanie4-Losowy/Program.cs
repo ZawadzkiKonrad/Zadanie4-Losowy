45f14e3d05af36e4b7e46e2507d9cc3850ab88ca
ZawadzkiKonrad <zaw_kon@o2.pl>
2022-02-01 08:52:54 +0100

    komentarz1

---
+++ Zadanie4-Losowy/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using Zadanie4_Losowy;
+
+String[] imie = { "Ania", "Kasia", "Basia", "Zosia" };
+String[] nazwisko = { "Kowalska", "Nowak" };
+
+
+
+List<User> listToSave = new List<User>();
+Random random = new Random();
+
+for (int i = 0; i < 100; i++)
+{
+    User user = new User();
+    user.LP = i + 1;
+
+    user.Imie = imie[random.Next(0, 3)];
+    user.Nazwisko = nazwisko[random.Next(0, 2)];
+    user.Rok = random.Next(1990, 2000);
+    listToSave.Add(user);
+
+}
+
+void SaveUsers(List<User> listToSave)
+{
+    DateTime thisDay = DateTime.Now;
+    var date = thisDay.ToString().Replace("-", "_").Replace(":","_").Replace(" ","-");
+    StreamWriter writer = new StreamWriter(@"C:\test\users-"+date+".csv", false);
+    if (writer != null)
+    {
+        writer.WriteLine(@"LP;IMIE;NAZWISKO;ROK");//Pierwszy wiersz to dodanie nazw kolumn
+        foreach (User user in listToSave)
+        {
+            writer.WriteLine(String.Format(@"{0};{1};{2};{3}", user.LP, user.Imie, user.Nazwisko, user.Rok));
+        }
+        writer.Close();
+    }
+}
+SaveUsers(listToSave);
---
+++ Zadanie4-Losowy/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie4_Losowy
+{
+    public class User
+    {
+        public int LP { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int Rok { get; set; }
+    }
+}
